0edd4060ae75a30b79774d1efb98f37e8e27f9ba
pYro-sudo <109588143+pYro-sudo@users.noreply.github.com>
2025-07-12 18:08:44 +0300

    Add files via upload
    нужны тесты еще, и там много липовых конфигураций, которые потом нужно менять

---
+++ src/main/java/by/losik/router/router/PostgresRouter.java
@@ -0,0 +1,10 @@
+package by.losik.router.router;
+
+import io.smallrye.mutiny.vertx.core.AbstractVerticle;
+import jakarta.enterprise.context.Dependent;
+import lombok.extern.slf4j.Slf4j;
+
+@Dependent
+@Slf4j
+public class PostgresRouter extends AbstractVerticle {
+}
